8610cb04f14176b8059371f29451028d82e90694
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-07-12 12:44:45 +0430

    init django project

---
+++ bilityab/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^$', 'bilityab.views.home', name='home'),
+    url(r'^events/$', 'bilityab.views.events', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
---
+++ bilityab/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html', {
+        # 'PageTitle': "bilityab",
+    })
+
+def events(request):
+    return render(request, 'sport-event.html', {
+        # 'PageTitle': "bilityab",
+    })
