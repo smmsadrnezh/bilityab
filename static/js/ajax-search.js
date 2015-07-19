86b797fe2e3878e0492ce23a75e44454d28cff0e
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-07-19 22:03:27 +0430

    favicon and search ajax

---
+++ static/js/ajax-search.js
@@ -0,0 +1,10 @@
+var availableTags = [
+	"مسابقه والیبال ایران و آمریکا",
+	"مسابقه فوتبال ایران و ترکیه",
+	"کشتی پیشکسوتان جهان"
+];
+$( "#autocomplete" ).autocomplete({
+	source: availableTags,
+    delay: 400,
+    minLength: 2
+});
