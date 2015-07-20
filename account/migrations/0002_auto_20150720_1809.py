3fec4eeab3c47b0678dbd9f8f6e51aba9bdb4433
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-07-21 02:21:51 +0430

    ticket

---
+++ account/migrations/0002_auto_20150720_1809.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='customuser',
+            name='gender',
+            field=models.BooleanField(default=0),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='phone',
+            field=models.IntegerField(default=b'02188888888'),
+        ),
+    ]
