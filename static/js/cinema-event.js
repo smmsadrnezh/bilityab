6e17765778afe2ae791830ac1bad1d51bd0aba39
mehran <mehran_t95@yahoo.com>
2015-07-24 21:42:04 +0430

    cinema new page

--- static/js/cinema-event.js
+++ static/js/cinema-event.js
@@ -40,28 +40,68 @@ $(window).load(function () {
         your_comment.find('textarea').val('');
     });
 
-
     var seat_progress = $('#seat-select .progress');
     var step1 = seat_progress.find('div:nth-of-type(1)');
     var step2 = seat_progress.find('div:nth-of-type(2)');
     var step3 = seat_progress.find('div:nth-of-type(3)');
+    var cinemas = $('#cinemas');
     var show_times = $('#show-times');
+    var plans = $('#plans');
+    var current_step = step1;
+    var current_select = cinemas;
 
     $('#cinemas .item').on('click', function () {
         step1.removeClass('active').addClass('done');
-        step1.next().removeClass('half').addClass('done');
+        step1.next().removeClass('active').addClass('done');
+        step2.next().addClass('active');
         step2.addClass('active');
-        $(this).parent().fadeOut();
-        show_times.fadeIn();
+        current_step = step2;
+        cinemas.fadeOut(function () {
+            show_times.fadeIn();
+            current_select = show_times;
+        });
     });
 
     $('.show-time').on('click', function(){
-        step2.removeClass('active').addClass('complete');
-        setTimeout(function () {
-            step3.removeClass('disabled').addClass('active');
-        }, 600);
-        $(this).parent().fadeOut();
-        $('#plans').fadeIn();
+        step2.removeClass('active').addClass('done');
+        step2.next().removeClass('active').addClass('done');
+        step3.addClass('active');
+        current_step = step3;
+        show_times.fadeOut(function () {
+            plans.fadeIn();
+            current_select = plans;
+        });
+    });
+
+    step1.on('click', function () {
+        if(current_step == step1)
+            return false;
+        current_select.fadeOut(function () {
+            cinemas.fadeIn();
+            current_select = cinemas;
+        });
+
+        step1.removeClass('done').addClass('active');
+        step1.next().removeClass('done').addClass('active');
+
+        step2.removeClass('done').removeClass('active');
+        step2.next().removeClass('done').removeClass('active');
+
+        step3.removeClass('active');
+    });
+
+    step2.on('click', function () {
+        if(current_step == step2 || current_step == step1)
+            return false;
+        current_select.fadeOut(function () {
+            show_times.fadeIn();
+            current_select = show_times;
+        });
+
+        step2.removeClass('done').addClass('active');
+        step2.next().removeClass('done').addClass('active');
+
+        step3.removeClass('active');
     });
 
 });
