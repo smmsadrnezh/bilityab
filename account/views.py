63d2482b450ec23c5249ed1e810eb5b9aa3a14be
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-08-01 16:41:42 +0430

    migrate

--- account/views.py
+++ account/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.template.loader import get_template
+from django.core.context_processors import csrf
 # Create your views here.
+
+
+def login(request):
+    invalid_html = ""
+
+    if request.method == "POST":
+        user = auth.authenticate(username=request.POST.get('username', ''), password=request.POST.get('password', ''))
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect(request.POST.get)
+        else:
+            invalid_html = get_template('invalid.html').render()
+
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'login.html', dict(c, **{'PageTitle': " - Login", 'invalid_html': invalid_html}))
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
 
 
 def profile_edit(request, user_id):
