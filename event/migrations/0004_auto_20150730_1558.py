77a2977ea7f182a998c9249ddb4fbfa9e5b19537
m3hran <mehran_t95@yahoo.com>
2015-08-02 16:45:44 +0430

    sport page

---
+++ event/migrations/0004_auto_20150730_1558.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('event', '0003_auto_20150730_1550'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='categories',
+            name='parent',
+        ),
+        migrations.AddField(
+            model_name='categories',
+            name='parent',
+            field=models.ManyToManyField(to='event.Categories', null=True),
+        ),
+    ]
