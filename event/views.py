7b77ee87c294bf7b4c7658106f85ca4565071e5d
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-08-03 06:07:34 +0430

    additional fields for sport and cinema

--- event/views.py
+++ event/views.py
@@ -3,7 +3,7 @@
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
-from event.models import Event,Categories
+from event.models import Event, Categories, Sport, Movie, Concert
 from bilityab.change_date import ChangeDate
 
 
@@ -15,21 +15,55 @@ def events(request):
 
 
 def add_event(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated() and request.user.is_organizer:
         if request.is_ajax():
-            if(request.POST.get('event-title', '') != "" and request.POST.get('event-description', '') != "" and request.POST.get('event-type', '') != "" and request.POST.get('event-capacity', '') != "" and request.POST.get('event-address', '') != ""):
-                Event(title=request.POST.get('event-title', ''),
-                    description=request.POST.get('event-description', ''),
-                    category_id=request.POST.get('event-type', ''),
-                    capacity=request.POST.get('event-capacity', ''),
-                    address=request.POST.get('event-address', ''),
-                    event_organizer_id=request.user.id,
-                    photo="jingili.jpg"
+            # insert event and it's additional information to database
+            if (request.POST.get('event-title', '') != "" and request.POST.get('event-description',
+                                                                               '') != "" and request.POST.get(
+                    'event-type', '') != "" and request.POST.get('event-capacity', '') != "" and request.POST.get(
+                    'event-address', '') != ""):
+                event = Event(title=request.POST.get('event-title', ''),
+                      description=request.POST.get('event-description', ''),
+                      category_id=request.POST.get('event-type', ''),
+                      capacity=request.POST.get('event-capacity', ''),
+                      address=request.POST.get('event-address', ''),
+                      event_organizer_id=request.user.id,
+                      photo="jingili.jpg"
+                )
+                event.save()
+                if (request.POST.get('event-home-team', '') != "" and request.POST.get('event-away-team', '') != ""):
+                    Sport(
+                        event_id = event.id,
+                        away_team=request.POST.get('event-home-team', ''),
+                        home_team=request.POST.get('event-away-team', '')
                     ).save()
+                elif (request.POST.get('event-director', '') != "" and request.POST.get('event-actors',
+                                                                                        '') != "" and request.POST.get(
+                        'event-year', '') != "" and request.POST.get('event-story-summary', '') != ""):
+                    Movie(
+                        event_id = event.id,
+                        director=request.POST.get('event-director', ''),
+                        actors=request.POST.get('event-actors', ''),
+                        year=request.POST.get('event-year', ''),
+                        story_summary=request.POST.get('event-story-summary', '')
+                    ).save()
+                elif (request.POST.get('event-vocalist', '') != "" and request.POST.get('event-musicians',
+                                                                                        '') != "" and request.POST.get(
+                        'event-music-group', '') != ""):
+                    Concert(
+                        event_id = event.id,
+                        group_name=request.POST.get('event-music-group', ''),
+                        vocalist=request.POST.get('event-vocalist', ''),
+                        musicians=request.POST.get('event-musicians', '')
+                    ).save()
+
+                # redirect to site homepage
                 return HttpResponse(1)
             else:
+                # raise exception to user
                 return HttpResponse(0)
         else:
+            # add new event template for organizer
             return render(request, 'add-event.html', {
                 'logged_in': request.user.is_authenticated(),
                 'categories': Categories.objects.all()
