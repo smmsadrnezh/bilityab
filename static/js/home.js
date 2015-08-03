779ed9e30f667e7997ed294e25c76e2fcf325ac2
anaeimian <arman88207136>
2015-08-03 20:32:53 +0430

    organizer page

--- static/js/home.js
+++ static/js/home.js
@@ -10,19 +10,34 @@
         window.location.href = "events/cinema/"+$(this).attr('event_id');
     });
 
-    var sportImage = $(".sport");
-    sportImage.on('click', function () {
-        window.location.href = "events/sport/1";
+    var sport_events = $('.sport').closest('.event');
+    sport_events.on('click', function () {
+        window.location.href = "events/sport/"+$(this).attr('event_id');
     });
 
-    var tourismImage = $(".tourism");
-    tourismImage.on('click', function () {
-        window.location.href = "events/tourism/1";
+    var tourism_events = $('.tourism').closest('.event');
+    tourism_events.on('click', function () {
+        window.location.href = "events/tourism/"+$(this).attr('event_id');
     });
 
-    var musicImage = $(".music");
-    musicImage.on('click', function () {
-        window.location.href = "events/music/1";
+    var music_events = $('.music').closest('.event');
+    music_events.on('click', function () {
+        window.location.href = "events/music/"+$(this).attr('event_id');
     });
+
+    //var sportImage = $(".sport");
+    //sportImage.on('click', function () {
+    //    window.location.href = "events/sport/1";
+    //});
+    //
+    //var tourismImage = $(".tourism");
+    //tourismImage.on('click', function () {
+    //    window.location.href = "events/tourism/1";
+    //});
+    //
+    //var musicImage = $(".music");
+    //musicImage.on('click', function () {
+    //    window.location.href = "events/music/1";
+    //});
 
 })(jQuery);
