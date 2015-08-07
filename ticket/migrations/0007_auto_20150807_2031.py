e8d0de29446201f2ba32fb6b1e0f2e1b6fea78e2
m3hran <mehran_t95@yahoo.com>
2015-08-08 00:34:28 +0430

    completing ticket buying

---
+++ ticket/migrations/0007_auto_20150807_2031.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ticket', '0006_auto_20150807_2029'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='ticketposition',
+            name='section',
+            field=models.PositiveIntegerField(blank=True, null=True),
+        ),
+    ]
