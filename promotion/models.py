1bc0ba0851fc5eaa959ac3e51f4767b17983d932
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-08-13 01:33:06 +0430

    promotion db migrate

--- promotion/models.py
+++ promotion/models.py
@@ -2,25 +2,14 @@ from django.db import models
 
 from account.models import CustomUser
 from event.models import Event
+from event.models import Showtime
 
 
 class Promotion(models.Model):
-    plan_name = models.CharField(unique=True, max_length=30)
+    showtime = models.OneToOneField(Showtime)
     discount = models.PositiveSmallIntegerField()
     remaining = models.PositiveSmallIntegerField()
-
-    def __str__(self):
-        return "%s" % self.plan_name
-
-
-class TicketPromotion(models.Model):
-    promotion = models.ForeignKey(Promotion)
-    user = models.ForeignKey(CustomUser)
-    event = models.ForeignKey(Event)
     issued_time = models.DateTimeField()
 
-    class Meta:
-        unique_together = (("promotion", "user", "event"),)
-
     def __str__(self):
-        return "promotion: %d \n event: %d" % (self.remaining, self.discount)
+        return "%s" % self.showtime
