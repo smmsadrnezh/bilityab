b80a91f4c42c8e66e6948e2f9b2bdf06acaf7056
m3hran <mehran_t95@yahoo.com>
2015-08-14 21:03:50 +0430

    recover password

--- account/models.py
+++ account/models.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User, UserManager
 
 
 class CustomUser(User):
-    """Consider that this class inherit django user class.
-    user class fields:  username, email, password, is_staff, is_active, date_joined and much more. """
-
     balance = models.FloatField(default=0)
     is_organizer = models.BooleanField(default=0)
     gender = models.BooleanField(default=0)
@@ -14,3 +11,8 @@ class CustomUser(User):
 
     # Use UserManager to get the create_user method, etc.
     objects = UserManager()
+
+
+class RecoverRequests(models.Model):
+    user = models.OneToOneField(CustomUser)
+    random_num = models.CharField(max_length=30)
