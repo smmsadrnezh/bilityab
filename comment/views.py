5f7f8e8e0e1a07c183b67a1876b1a642d5706dc7
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-08-17 01:00:22 +0430

    comment

--- comment/views.py
+++ comment/views.py
@@ -2,11 +2,11 @@ from comment.models import Comment,CommentEvent
 from account.models import CustomUser
 
 def comments(event_id):
-    print(Comment.objects.get(id=2).time)
+    print(Comment.objects.get(id=2).text)
     comment_type_list = []
     for comment_id in CommentEvent.objects.filter(event_id=event_id):
-        for comment in Comment.objects.filter(id=comment_id.comment_id):
-            writer = CustomUser.objects.get(id=comment.user_id).username
-            comment_type_list.append((comment, writer))
+        comment = Comment.objects.get(id=comment_id.comment_id)
+        writer = CustomUser.objects.get(id=comment.user_id).username
+        comment_type_list.append((comment, writer))
 
     return comment_type_list
