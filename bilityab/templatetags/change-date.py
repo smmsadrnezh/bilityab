487bec58229f2d28032ca079470880bfa075dfd1
m3hran <mehran_t95@yahoo.com>
2015-08-17 11:01:26 +0430

    gender symbol in registration form

--- bilityab/templatetags/change-date.py
+++ bilityab/templatetags/change-date.py
@@ -10,5 +10,7 @@ register = template.Library()
 def cut(value, arg):
     if arg == 'without_week_day':
         return ChangeDate().get_persian_date(value)
+    elif arg == 'date-time':
+        return ChangeDate().get_persian_date_time(value)
     else:
         return ChangeDate().get_persian_date_week_day(value)
